2af97eada278376154d5ad460315a4205373be51
Alex Eckermann <alex+gh@alexeckermann.com>
2018-07-04 18:02:08 +0930

    Fixes path issue in manual test js and added placeholder css to manual tests.

--- tests/manual/emptyness.js
+++ tests/manual/emptyness.js
@@ -1,16 +1,24 @@
 import ClassicEditor from '@ckeditor/ckeditor5-editor-classic/src/classiceditor';
 
 import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
-import Emptyness from '../src/emptyness';
+import Emptyness from '../../src/emptyness';
 
 ClassicEditor
 	.create( document.querySelector( '#editor' ), {
 		plugins: [ Paragraph, Emptyness ],
-		toolbar: [ 'paragraph' ]
+		toolbar: [ ]
 	} )
 	.then( editor => {
 		window.editor = editor;
+		
+		editor.on( 'change:isEmpty', () => {
 
-		const destroyButton = document.getElementById( 'destroy-editor-button' );
-		destroyButton.addEventListener( 'click', () => editor.destroy() );
+			if ( editor.isEmpty ) {
+				element.setAttribute( 'data-empty', true );
+			} else {
+				element.removeAttribute( 'data-empty' );
+			}
+
+		} );
+		
 	} );
--- tests/manual/emptyness.md
+++ tests/manual/emptyness.md
@@ -1,4 +1,4 @@
-1. Upon loading the page the editor should contain the text `The editor is empty`.
+1. Upon loading the page the editor should contain the placeholder text: `The editor is empty`.
 
 2. Type something and notice that the placeholder text disappears as expected.
 
